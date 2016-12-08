31e1012ca70edadbf07d4885e8537e052637f835
Garrett K <gkam222@gmail.com>
2016-12-08 15:01:58 -0600

    slight update

---
+++ btree.cpp
@@ -0,0 +1,134 @@
+//btree.tc
+//#include "btnode.cpp"
+//#include "indbuff.cpp"
+#include "btree.h"
+#include <iostream>
+#include "SequenceSet.h"
+using namespace std;
+const int MaxHeight = 5;
+template <class keyType>
+BTree<keyType>::BTree(int order, SequenceSet r, int keySize)
+{
+	root = r;
+	Height = 1;
+	Order = order;
+	PoolSize = MaxHeight*2;
+	Nodes = new BTNode * [PoolSize];
+	BTNode::InitBuffer(Buffer,order);
+	Nodes[0] = &Root;
+}
+
+template <class keyType>
+BTree<keyType>::~BTree()
+{
+	Close();
+	delete Nodes;
+}
+
+template <class keyType>
+int BTree<keyType>::Insert (const keyType key, const int recAddr)
+{
+	int result; int level = Height-1;
+	int newLargest=0; keyType prevKey, largestKey;
+	BTNode * thisNode, * newNode, * parentNode;
+	thisNode = FindLeaf (key);
+
+	// test for special case of new largest key in tree
+	if (key > thisNode->LargestKey())
+		{newLargest = 1; prevKey=thisNode->LargestKey();}
+
+	result = thisNode -> Insert (key, recAddr);
+
+	// handle special case of new largest key in tree
+	if (newLargest)
+		for (int i = 0; i<Height-1; i++)
+		{
+			Nodes[i]->UpdateKey(prevKey,key);
+			if (i>0) Store (Nodes[i]);
+		}
+
+	while (result==-1) // if overflow and not root
+	{
+		//remember the largest key
+		largestKey=thisNode->LargestKey();
+		// split the node
+		newNode = NewNode();
+		thisNode->Split(newNode);
+		Store(thisNode); Store(newNode);
+		level--; // go up to parent level
+		if (level < 0) break;
+		// insert newNode into parent of thisNode
+		parentNode = Nodes[level];
+		result = parentNode->UpdateKey
+			(largestKey,thisNode->LargestKey());
+		result = parentNode->Insert
+			(newNode->LargestKey(),newNode->RecAddr);
+		thisNode=parentNode;
+	}
+	Store(thisNode);
+	if (level >= 0) return 1;// insert complete
+	// else we just split the root
+	int newAddr = BTreeFile.Append(Root); // put previous root into file
+	// insert 2 keys in new root node
+	Root.Keys[0]=thisNode->LargestKey();
+	Root.RecAddrs[0]=newAddr;
+	Root.Keys[1]=newNode->LargestKey();
+	Root.RecAddrs[1]=newNode->RecAddr;
+	Root.NumKeys=2;
+	Height++;
+	return 1;
+}
+
+template <class keyType>
+int BTree<keyType>::Remove (const keyType key, const int recAddr)
+{
+	// left for exercise
+	return -1;
+}
+
+template <class keyType>
+int BTree<keyType>::Search (const keyType key, const int recAddr)
+{
+	BTNode * leafNode;
+	leafNode = FindLeaf (key);
+	return leafNode -> Search (key, recAddr);
+}
+
+template <class keyType>
+BTreeNode<keyType> * BTree<keyType>::FindLeaf (const keyType key)
+// load a branch into memory down to the leaf with key
+{
+	int recAddr, level;
+	for (level = 1; level < Height; level++)
+	{
+		recAddr = Nodes[level-1]->Search(key,-1,0);//inexact search
+		Nodes[level]=Fetch(recAddr);
+	}
+	return Nodes[level-1];
+}
+
+template <class keyType>
+BTreeNode<keyType> * BTree<keyType>::NewNode ()
+{// create a fresh node, insert into tree and set RecAddr member
+	BTNode * newNode = new BTNode(Order);
+	int recAddr = BTreeFile . Append(*newNode);
+	newNode -> RecAddr = recAddr;
+	return newNode;
+}
+
+template <class keyType>
+BTreeNode<keyType> * BTree<keyType>::Fetch(const int recaddr)
+{// load this node from File into a new BTreeNode
+	int result;
+	BTNode * newNode = new BTNode(Order);
+	result = BTreeFile.Read (*newNode, recaddr);
+	if (result == -1) return NULL;
+	newNode -> RecAddr = result;
+	return newNode;
+}
+
+template <class keyType>
+int BTree<keyType>::Store (BTreeNode<keyType> * thisNode)
+{
+	return BTreeFile.Write(*thisNode, thisNode->RecAddr);
+}
--- btree.h
+++ btree.h
@@ -1,1 +1,51 @@
 #pragma once
+// btree.h
+
+#ifndef BTREE_H
+#define BTREE_H
+
+template <class keyType>
+class BTree;
+
+//#include "btnode.h"
+//#include "recfile.h"
+//#include "fixfld.h"
+//#include "indbuff.h"
+#include "SequenceSet.h"
+using namespace std;
+// btree needs to be able to pack, unpack, read and
+// 	write index records
+// 	also, handle the insert, delete, split, merge,
+//	growth of the tree
+//	a pool of nodes will be managed
+//	keep at least a branch of the tree in memory
+//	
+
+template <class keyType>
+class BTree
+{
+private:
+	struct Node
+	{
+		int count;
+		Node *left;
+		Node *right;
+	};
+	Node *root;
+
+	int Height; // height of tree
+	int Order; // order of tree
+	int PoolSize;
+
+	SequenceSet root;
+
+public:
+	BTree(int order, SequenceSet r, int keySize = sizeof(keyType));
+	~BTree();
+	int Insert(const keyType key, const int recAddr);
+	int Remove(const keyType key, const int recAddr = -1);
+	int Search(const keyType key, const int recAddr = -1);
+
+};
+
+#endif /* end of BPT_H */
